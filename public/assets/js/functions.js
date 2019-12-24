5e6ebe1b4c2dda8a9fffd7cb603c16b59f78eec2
Jesus F <sherman589@gmail.com>
2019-12-24 11:50:31 -0400

    refactory orderDetails calulos, cunciones javascript

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -515,7 +515,6 @@ function calculoUsbBudget(){
     var orderSaleUsd        = $('#order_sale_usd').val();
     var orderPurchaseUsd    = $('#order_purchase_usd').val();
     var totalEstCharges     = $('#total_est_charges').val();
-    var estCharges          = $('#est_charges').val();
     var comToPay            = $('#comtopay').val();
     var comToRecive         = $('#comtoreceive').val();
     var usbBudget           = 0;
