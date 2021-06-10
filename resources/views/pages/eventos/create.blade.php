5d89ee9cf3f758f3222d536f541109153938e517
J Fernandez <sherman589@gmail.com>
2021-06-10 07:44:07 -0400

    inicio formulario eventos

--- resources/views/pages/eventos/create.blade.php
+++ resources/views/pages/eventos/create.blade.php
@@ -1,15 +1,21 @@
 <div class="modal fade" id="create" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-	<div class="modal-dialog modal-lg" role="document">
+	<div class="modal-dialog" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
 				<h4 class="modal-title" id="myModalLabel"><i class="icon icon-calendar"></i> Nuevo Evento</h4>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 			</div>
 			<div class="modal-body">
+				<div class="col-12 form-group">
+					{!! Form::text('name', null, ['class'=>'form-control','placeholder'=>'título']) !!}
+				</div>
+				<div class="col-12">
+					{!! Form::textarea('name', null, ['class'=>'form-control','placeholder'=>'Descripción']) !!}
+				</div>
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-default" data-dismiss="modal">{{ __('Close')}}</button>
-				<button type="submit" class="btn btn-primary"><i class="icon-save mr-2"></i>{{ __('Save data')}}</button>
+				<button type="submit" class="btn btn-primary"><i class="icon-save mr-2"></i>{{ __('Guardar')}}</button>
 			</div>
 		</div>
 	</div>
